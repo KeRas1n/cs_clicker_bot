b1747a0b98508701a9483daea7490adb0ff03a6e
KeRas1n <kirill2006915@gmail.com>
2024-07-06 17:53:38 +0200

    small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import defaultKtKnife from './assets/defaultKtKnife.png'
 
-const tg = window.Telegram.WebApp;
+const tg = (window as any).Telegram.WebApp;
 
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [tap, setTap] = useState(1);
+  //const [tap, setTap] = useState(1);
 
   useEffect(()=> {
     tg.ready();
@@ -20,7 +20,7 @@ function App() {
   return (
     <>
       <div className='h-full flex flex-col justify-center items-center h-screen main'>
-        <div className='relative knife-btn' onClick={() => setCounter((prev) => prev + tap)}>
+        <div className='relative knife-btn' onClick={() => setCounter((prev) => prev + 1)}>
           <img src={defaultKtKnife} className='w-[200px] z-[999] absolute'/>
           <img src={defaultKtKnife} className='w-[200px] blur-lg top-0 z-0 '/>
         </div>
