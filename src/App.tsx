ec57c008e3c7d3e2433e0eeeb94133990306c5de
KeRas1n <kirill2006915@gmail.com>
2024-07-06 17:38:25 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import defaultKtKnife from './assets/defaultKtKnife.png'
+import dust from './assets/Bdust.png'
+
+const tg = window.Telegram.WebApp;
+
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [tap, setTap] = useState(1);
+
+  useEffect(()=> {
+    tg.ready();
+  }, [])
+
+  const onClose = () => {
+    tg.close()
+  }
+
+  return (
+    <>
+      <div className='h-full flex flex-col justify-center items-center h-screen main'>
+        <div className='relative knife-btn' onClick={() => setCounter((prev) => prev + tap)}>
+          <img src={defaultKtKnife} className='w-[200px] z-[999] absolute'/>
+          <img src={defaultKtKnife} className='w-[200px] blur-lg top-0 z-0 '/>
+        </div>
+        <div className='block text-3xl'>{counter}</div>
+        <button className='btn mt-5' onClick={onClose}>Close</button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
